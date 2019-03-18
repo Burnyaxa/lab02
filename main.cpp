539a1d19df3334eb397c40ff56a9efd06afb08b3
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-03-18 19:13:12 +0200

    base

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "method.h"
+
+void main(int argc, char* argv[])
+{
+
+}
---
+++ method.cpp
@@ -0,0 +1,10 @@
+vector <string> charTOvecstring (char*argv[])
+{
+	vector <string> str;
+	for (int i = 0; argv[i]; i++)
+	{
+		string s(argv[i]);
+		str[i] = s;
+	}
+	return str;
+}
