e30ca67a4da91b54009ffaa977eaeb9d7aa12d43
Artem Supryhan <suprigan.artem1@gmail.com>
2019-03-26 13:15:28 +0200

    Update shunting-yard.cpp

--- shunting-yard.cpp
+++ shunting-yard.cpp
@@ -34,43 +34,47 @@ int shuntingYard(string row){
 	stack <int> output;
 	stack <char> oper;
 	string token, integer, temp;
-	int first, second;
-	char out;
+	int first, second, result;
+	string out;
 	for (int i = 0; i < row.size(); i++){
-			token = row[i];
-			if (isdigit(token[0])){
-				integer += row[i];
-				continue;
-			}
-			if (!isdigit(token[0]) && integer.size()){
+		token = row[i];
+		if (isdigit(token[0])){
+			integer += row[i];
+			if (i == row.size() - 1){
 				output.push(stoi(integer));
 				integer.clear();
 			}
-			if (!isdigit(token[0]) && !isspace(token[0])){
-				if (oper.empty()){
+			continue;
+		}
+		if (!isdigit(token[0]) && integer.size()){
+			output.push(stoi(integer));
+			integer.clear();
+		}
+		if (!isdigit(token[0]) && !isspace(token[0])){
+			if (oper.empty()){
+				oper.push(token[0]);
+			}
+			else{
+				//oper.top(temp);
+				operation current(token[0]);
+				//operation prev(temp[0]);
+				operation prev(oper.top());
+				if (prev.getPriority() > current.getPriority()){
+					out = oper.top();
+					oper.pop();
+					second = output.top();
+					output.pop();
+					first = output.top();
+					output.pop();
+					output.push(calculate(first, second, out[0]));
 					oper.push(token[0]);
 				}
 				else{
-					//oper.top(temp);
-					operation current(token[0]);
-					//operation prev(temp[0]);
-					operation prev(oper.top());
-					if (prev.getPriority() > current.getPriority()){
-						out = oper.top();
-						oper.pop();
-						second = output.top();
-						output.pop();
-						first = output.top();
-						output.pop();
-						output.push(calculate(first, second, out));
-						oper.push(token[0]);
-					}
-					else{
-						oper.push(token[0]);
-					}
+					oper.push(token[0]);
 				}
 			}
 		}
+	}
 	while (!oper.empty()){
 		out = oper.top();
 		oper.pop();
@@ -78,9 +82,9 @@ int shuntingYard(string row){
 		output.pop();
 		first = output.top();
 		output.pop();
-		output.push(calculate(first, second, out));
+		output.push(calculate(first, second, out[0]));
 	}
-	out = output.top();
+	result = output.top();
 	output.pop();
-	return (int)out;
+	return result;
 }
