9361dfa8e836c9e9a5bdeff283490b4e6662869e
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-03-28 09:52:45 +0200

    ConvDoub обрабатывает строку и конвертирует дублирующися элементы , либо вызывает ошибку при невеном вводе.

--- method.cpp
+++ method.cpp
@@ -17,6 +17,55 @@ string cmdString(int argc, char*argv[])
 		string a(argv[i]);
 		str = str + a + " ";
 	}
-	
+
 	return str;
+}
+
+
+string ConvDoub (string &line)
+{
+	int size = line.size();
+	for (int i = 1; i < size; i++)
+	{
+		if (line[i - 1] == line[i])
+		{
+			if (isdigit(line[i-1])) { continue; }
+			if (line[i-1] == '(' || line[i - 1] == ')') { continue; }
+
+			if (readOper(line[i - 1]))
+			{
+				while (line[i - 1] == line[i])
+				{
+					int temp = readOper(line[i - 1]);
+					if (temp > 2)
+					{
+						cout << "there not correct input\n";
+						string err = "it is wrong: ";
+						err = err + line[i - 1] + line[i];
+						return err;
+					}
+					if (temp == 1) { line.erase(i - 1, 1); size--; continue; }
+					if (temp == 2)
+					{
+						if (readOper(line[i + 1]))
+						{
+							if (temp > 2)
+							{
+								cout << "there not correct input\n";
+								string err = "it is wrong: ";
+								err = err + line[i - 1] + line[i];
+								return err;
+							}
+							if (temp == 1) { line.erase(i - 1, 1); size--; continue; }
+							if (temp == 2) { line.erase(i - 1, 2); size -= 2; continue; }
+						}
+						else { line[i - 1] = '+'; line.erase(i, 1); size--; continue; }
+					}
+				}
+				continue;
+			}
+			cout << "problem in ConvDoub\ni: "<<i<<endl;
+		}
+	}
+	return line;
 }
--- method.h
+++ method.h
@@ -3,5 +3,6 @@
 #include <vector>
 #include <string>
 using namespace std;
-vector <string> charTOvecstring (char*[])
-string cmdString (int , char*[])
+vector <string> charTOvecstring (char*[]);
+string cmdString (int , char*[]);
+string ConvDoub (string &);
